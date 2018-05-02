4b67dec5848d022c3750891531e01f54ffc6d074
Cryptolemming <aliayoub22@gmail.com>
2018-05-02 13:20:52 -0400

    initial setup and commit

---
+++ src/components/crypto-currency-trader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './crypto-currency-trader.css';
+
+export class CryptoCurrencyTrader extends React.Component {
+
+	render() {
+		return (
+			<p>Hi</p>
+		);
+	}
+
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CryptoCurrencyTrader } from './components/crypto-currency-trader';
+
+import './index.css';
+
+ReactDOM.render(
+	<CryptoCurrencyTrader />,
+	document.getElementById('root')
+);
