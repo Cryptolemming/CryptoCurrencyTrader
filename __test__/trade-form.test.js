0158c2f3d752c42b6c72605df9e5b18e93552a39
Cryptolemming <aliayoub22@gmail.com>
2018-05-04 01:53:06 -0400

    further jest testing setup

--- __test__/trade-form.test.js
+++ __test__/trade-form.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
-import TradeForm from '../src/components/trade-form';
+import { TradeForm } from '../src/components/trade-form';
 
 describe('<TradeForm />', () => {
     it('Renders without crashing', () => {
